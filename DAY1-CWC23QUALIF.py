490def042e1988bcd5aae020de86bb60d39e121b
Vivaadhcoding <ayaan2015gwl@gmail.com>
2024-09-29 16:45:20 +0530

    Add files via upload

---
+++ DAY1-CWC23QUALIF.py
@@ -0,0 +1,9 @@
+# cook your dish here
+# Input the total points scored by the team
+X = int(input())
+
+# Check if the team has scored 12 or more points to qualify
+if X >= 12:
+    print("Yes")
+else:
+    print("No")
